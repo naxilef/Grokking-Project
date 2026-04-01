62aff68cb2c5d7fe7aa43d4e2b5f08edbe7854d2
naxilef <fphamponton@hotmail.com>
2026-04-01 13:49:47 -0400

    Sparce model + main use cude if available

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ import torch
 from data.modular_arith import make_modular_dataset, modular_addition, split_dataset
 from models.modular_mlp import ModularMLP
 
-def train(model, x_train, y_labels, loss_fct, optimizer, epochs = 1000):
+def train(model, x_train, y_labels, loss_fct, optimizer, device, epochs = 1000):
     model.train()
     losses = [] 
+
+    x_train = x_train.to(device)
+    y_labels = y_labels.to(device)
 
     for epoch in range(epochs):
         optimizer.zero_grad()
@@ -19,12 +22,15 @@ def train(model, x_train, y_labels, loss_fct, optimizer, epochs = 1000):
         optimizer.step()
 
         losses.append(loss.item())
-        if (epoch + 1) % 10 == 0:
-            print(f'Epoch [{epoch + 1}/{epochs}], Loss: {loss.item():.4f}')
+        if (epoch + 1) % 100 == 0:
+            print(f'Epoch [{epoch + 1}/{epochs}], Loss: {loss.item():.8f}')
 
     return losses    
 
-def evaluate(model, x_test, y_test):
+def evaluate(model, x_test, y_test, device):
+    x_test = x_test.to(device)
+    y_test = y_test.to(device)
+
     with torch.no_grad():
         outputs = model(x_test)
         predictions = torch.argmax(outputs, dim=1)
@@ -39,29 +45,35 @@ def evaluate(model, x_test, y_test):
 def run(p, hidden_dim, alpha=0.5, seed=0, activation="quadratic", lr=1e-3, epochs=1000):
     torch.manual_seed(seed)
 
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(f"Using device: {device}")
+
+    if device.type == "cuda":
+        print(f"GPU: {torch.cuda.get_device_name(0)}")
+
     tensors = make_modular_dataset(p, modular_addition)
     train_dataset, test_dataset = split_dataset(tensors.x, tensors.y, alpha=alpha,seed=seed)
 
-    model = ModularMLP(p=p, hidden_dim=hidden_dim, activation=activation)
+    model = ModularMLP(p=p, hidden_dim=hidden_dim, activation=activation).to(device)
     loss_fct = torch.nn.MSELoss()
     optimizer = torch.optim.SGD(model.parameters(), lr=lr)
 
-    train(model=model, x_train=train_dataset.x, y_labels=train_dataset.y, loss_fct=loss_fct, optimizer=optimizer, epochs=epochs)
+    train(model=model, x_train=train_dataset.x, y_labels=train_dataset.y, loss_fct=loss_fct, optimizer=optimizer, device=device, epochs=epochs)
 
-    results = evaluate(model=model, x_test=test_dataset.x, y_test=test_dataset.y)
+    results = evaluate(model=model, x_test=test_dataset.x, y_test=test_dataset.y, device=device)
 
     return model, results
 
 
 if __name__ == "__main__":
     model, results = run(
-        p=11,
-        hidden_dim=32,
-        alpha=0.5,
-        seed=0,
+        p=97,
+        hidden_dim=512,
+        alpha=0.45,
+        seed=42,
         activation="quadratic",
         lr=1e-2,
-        epochs=100000,
+        epochs=2000,
         )
     
 
